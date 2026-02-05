ef58c83d1bf09299b7d262e1cb1f64172eaf782b
DRANIK20073 <2007gruntov@gmail.com>
2026-02-05 15:12:48 +0300

    Добавил элменты в окне пользователя

--- WhiteSpace/Pages/UserHomePage.xaml.cs
+++ WhiteSpace/Pages/UserHomePage.xaml.cs
@@ -1,12 +1,50 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
 
 namespace WhiteSpace.Pages
 {
-    public partial class UserHomePage : Page
+    public partial class UserHomePage : Page, INotifyPropertyChanged
     {
+        private string _userGreeting = "Добро пожаловать!";
+
+        public string UserGreeting
+        {
+            get => _userGreeting;
+            set
+            {
+                _userGreeting = value;
+                OnPropertyChanged();
+            }
+        }
+
         public UserHomePage()
         {
-            InitializeComponent(); 
+            InitializeComponent();
+            DataContext = this;
+            LoadUserProfile();
+        }
+
+        private async void LoadUserProfile()
+        {
+            var service = new SupabaseService();
+            var profile = await service.GetMyProfileAsync();
+
+            if (profile != null && !string.IsNullOrEmpty(profile.Username))
+            {
+                UserGreeting = $"Здравствуйте, {profile.Username} 👋";
+            }
+            else
+            {
+                UserGreeting = "Здравствуйте!";
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
